03c418385a2cc83d23b0a536ab3a83343684ea10
Ash Ranjan <ranjanashvin@gmail.com>
2020-12-14 16:54:08 -0800

    base commands, working on custom commands next

--- n.py
+++ n.py
@@ -1,18 +1,111 @@
 import re
-import rstr
+import functools
+import importlib
+
+class Error:
+	def __init__(self, message, linenumber, char):
+		self.message = message
+		self.linenumber = linenumber
+		self.char = char
 
 # get lines, strip, and remove any emtpy lines
 lines = []
 with open("run.n", "r") as f:
 	lines = [line.strip() for line in f.readlines()]
-	lines = list(filter(None, lines))
 
-print(lines)
+
+end = False
+
+labels = {}
+
+imports = []
+
+specialcom = ["sendt", "if", "import", "exit"]
+
+# get the location of all quotes in te 
+def findQuotes(s):
+	out = []
+	for i,char in enumerate(s.replace("\\\"", "ee")):
+		if char == "\"":
+			if len(out) == 0:
+				out.append([i])
+			elif len(out[len(out)-1]) == 1:
+				out[len(out)-1].append(i)
+			else:
+				out.append([i])
+
+	return out
+
+def throwError(e):
+	print("Error at: " + str(e.linenumber) + ", " + str(e.char) + ": " + e.message)
+	exit()
+
+def inQuotes(l, q):
+	for i in q:
+		if l >= i[0] and l <= i[1]:
+			return True
+
+	return False
+
+def sReplace(s, f, r):
+	return re.sub(r'(' + f + r')(?=(?:[^"]|"[^"]*")*$)', r, s)
+
+def runCommand(c, l):
+	return runBaseCommand(c, l)
+
+def runBaseCommand(c, l):
+	if c.split(" ")[0].strip() == "sendt":
+		if not c.split(" ")[1] in labels:
+			throwError(Error("Label not found.", l + 1, 7))
+		else:
+			return labels[c.split(" ")[1]]
+	elif c.split(" ")[0].strip() == "exit":
+		exit()
+	elif c.split(" ")[0].strip() == "if":
+		if re.search(r'\((.+)\)', c) == None:
+			throwError(Error("Boolean not found", l, 4))
+		if eval(re.search(r'\((.+)\)', c).group(0).replace('&&', 'and').replace('||', 'or').replace('!', 'not ')):
+			return runCommand(c[re.search(r'\((.+)\)', c).span()[1] + 1 : len(c)].strip(), l)
+	elif c.split(" ")[0].strip() == "import":
+		works = True
+		try:
+			imports.append(import_module(c.split(" ")[1].strip()))
+		except:
+			works = False
+
+		if not works:
+			throwError(Error("Modlue not found", l, 8))
+		return l
+	else:
+		return l
+
+l = []
+
+for linenumb,line in enumerate(lines):
+	if line != "":
+		# check for comments and remove them
+		if ";" in line.replace("\\;", ""):
+			line = line.replace("\\;", u"\ufffc").split(";")[0].replace(u"\ufffc", "\\;")
+
+		# do some pre processing before running commands
+
+		# find the location of quotes
+		quotes = findQuotes(line)
+
+		# check for unbalanced quotes
+		if not all([len(l) == 2 for l in quotes]):
+			throwError(Error("Unmatched quote.", linenumb + 1, quotes[len(quotes) - 1][0] + 1))
+
+		# check if line declares a label
+		if line.startswith(">"):
+			labels[line[1:len(line)].strip()] = linenumb
+		else:
+			l.append(line)
+
+lines = l
 
 # linenumber, useful because it will be changed by sendt
 linenumb = 0
-
-end = False
 
 while True:
 	try:
@@ -22,10 +115,13 @@ while True:
 
 	if end:
 		exit()
-	# check for comments and remove them
-	if ";" in line.replace("\\;", ""):
-		line = line.replace("\\;", u"\ufffc").split(";")[0].replace(u"\ufffc", "\\;")
 
-	print(line)
+	if line != "":
+		
+		if functools.reduce(lambda x, y: x or y, [line.startswith(l) for l in specialcom]):
+			linenumb = runCommand(line, linenumb)
+				
+
+
 
 	linenumb += 1
