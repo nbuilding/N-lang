9a6e054a32b675718a7b9632008d55df4acb2cb1
SheepTester <ussa.say@gmail.com>
2020-12-20 18:13:28 -0800

    Get rollup working

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -1,12 +1,60 @@
 import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import copy from 'rollup-plugin-copy'
+import scss from 'rollup-plugin-scss'
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'output',
-    format: 'cjs'
-  },
-  plugins: [
-    typescript()
+const isProduction = process.env.NODE_ENV === 'production'
+
+export default (async () => {
+  const terser = isProduction && (await import('rollup-plugin-terser')).terser
+  return [
+    {
+      input: 'src/index.ts',
+      context: 'window',
+      output: {
+        file: 'dist/bundle.js',
+        format: 'iife',
+        inlineDynamicImports: true,
+        sourcemap: true,
+      },
+      preserveEntrySignatures: false,
+      plugins: [
+        typescript(),
+        nodeResolve(),
+        commonjs(),
+        scss({
+          output: 'dist/bundle.css',
+          outputStyle: isProduction && 'compressed',
+          sourceMap: true,
+        }),
+        isProduction && terser(),
+        copy({
+          targets: [
+            {
+              src: 'static/*',
+              dest: 'dist',
+            },
+            {
+              src: 'node_modules/monaco-editor/esm/vs/base/browser/ui/codicons/codicon.ttf',
+              dest: 'dist',
+            },
+          ],
+        }),
+      ],
+    },
+    {
+      input: 'node_modules/monaco-editor/esm/vs/editor/editor.worker.js',
+      context: 'self',
+      output: {
+        file: 'dist/editor.worker.js',
+        format: 'iife',
+        name: 'whatever',
+        sourcemap: true,
+      },
+      plugins: [
+        isProduction && terser(),
+      ],
+    },
   ]
-}
+})()
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,13 @@
 import * as monaco from 'monaco-editor'
 
+import './style.css'
+
 // Since packaging is done by you, you need
 // to instruct the editor how you named the
 // bundles that contain the web workers.
 (window as any).MonacoEnvironment = {
   getWorkerUrl: function (_moduleId: string, _label: string) {
-    return './editor.worker.bundle.js';
+    return './editor.worker.js';
   }
 }
 
