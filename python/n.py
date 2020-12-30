dfbbec33cda5217ce67f485195925044687509ee
Ash Ranjan <ranjanashvin@gmail.com>
2020-12-30 11:55:38 -0800

    New internal functions

--- python/n.py
+++ python/n.py
@@ -765,6 +765,31 @@ global_scope.add_native_function(
 	"int",
 	lambda number: ceil(number),
 )
+global_scope.add_native_function(
+	"charCode",
+	[("character", "char")],
+	"int",
+	lambda character: ord(character),
+)
+global_scope.add_native_function(
+	"intCode",
+	[("number", "int")],
+	"char",
+	lambda number: chr(number),
+)
+global_scope.add_native_function(
+	"charAt",
+	[("string", "str"), ("location", "int")],
+	"char",
+	lambda string, location: string[location],
+)
+global_scope.add_native_function(
+	"substring",
+	[("string", "str"), ("start", "int"), ("end", "int")],
+	"char",
+	lambda string, start, end: string[start : end],
+)
+
 
 def type_check(file, tree):
 	scope = global_scope.new_scope()
