4ec9d54d49585b9f6a69adfcba430542b407d4b9
Ashvin-Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2021-01-17 19:31:23 -0800

    its working but the cmd is not working

--- python/libraries/request.py
+++ python/libraries/request.py
@@ -1,7 +1,28 @@
 import requests
 import json
-import native_types
 import ast
+import aiohttp
+import asyncio
+from native_types import n_cmd_type, NMap, n_map_type
+
+botid = ""
+
+def setId(id):
+	global botid
+	botid = id
+
+async def sendMessage(channel, message, tts):
+	async with aiohttp.ClientSession() as session:
+		async with session.get(
+			f"https://discord.com/api/channels/{channel}/messages",
+			data=json.dumps({"content": message, "tts": tts}),
+			headers={'Content-Type': "application/json", "Authorization": f"Bot {botid}"},
+		) as response:
+			return {
+				"code": response.status,
+				"response": response.reason,
+				"text": await response.text(),
+			}
 
 #for gateways
 scope = None
@@ -9,15 +30,16 @@ scope = None
 def _prepare(sc):
 	scope = sc
 
-def get(args):
+async def get(args):
 	url = args[0]
+	headers = args[1]
+	async with aiohttp.ClientSession() as session:
+		async with session.get(requests.get(url, headers)) as r:
+			out = ast.literal_eval(r.text)
+			for key in out.keys():
+				out[key] = str(out[key])
 
-	r = requests.get(url)
-	out = ast.literal_eval(r.text)
-	for key in out.keys():
-		out[key] = str(out[key])
-
-	return {"code": r.status_code, "response": r.reason, "out": native_types.NMap(out)}
+			return {"code": r.status_code, "response": r.reason, "out": NMap(out)}
 
 def post(args):
 	url, content, *rest = args
@@ -29,4 +51,15 @@ def post(args):
 
 
 def _values():
-	return {"post": ("str", native_types.n_map_type.with_typevars(["str", "str"]), {"code": "int", "reason": "str", "text": "str"}), "get": ("str", native_types.n_map_type.with_typevars(["str", "str"]), {"code": "int", "reason": "str", "return": native_types.n_map_type.with_typevars(["str", "str"])})}
+	return {
+		"post": (
+			"str",
+			n_map_type.with_typevars(["str", "str"]),
+			{"code": "int", "reason": "str", "text": "str"}
+		),
+		"get": (
+			"str",
+			n_map_type.with_typevars(["str", "str"]),
+			n_cmd_type.with_typevars([{"code": "int", "reason": "str", "return": n_map_type.with_typevars(["str", "str"])}])
+		)
+	}
