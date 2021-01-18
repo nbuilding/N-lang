6d2fc7a65b1212f22b7447bc1257f7dc33f3297d
Ash Ranjan <ranjanashvin@gmail.com>
2021-01-18 11:29:30 -0800

    it works!

--- python/libraries/request.py
+++ python/libraries/request.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import ast
 import aiohttp
 import asyncio
 from native_types import n_cmd_type, NMap, n_map_type
@@ -11,25 +10,22 @@ scope = None
 def _prepare(sc):
 	scope = sc
 
-async def get(args):
-	url, headers = args
+async def get(url, headers):
 	async with aiohttp.ClientSession() as session:
-		async with session.get(requests.get(url, headers)) as r:
-			out = ast.literal_eval(await r.text)
+		async with session.get(url, headers=headers) as r:
+			out = json.loads(await r.text())
 			for key in out.keys():
 				out[key] = str(out[key])
 
-			return {"code": r.status_code, "response": r.reason, "out": NMap(out)}
+			return {"code": r.status, "response": r.reason, "return": NMap(out)}
 
-async def post(args):
-	url, content, headers = args
-	
+async def post(url, content, headers):
 	async with aiohttp.ClientSession() as session:
-		async with session.get(requests.post(url, data=json.dumps(content), headers=headers)) as r:
+		async with session.post(url, data=json.dumps(content), headers=headers) as r:
 			for key in out.keys():
 				out[key] = str(out[key])
 
-			return {"code": r.status_code, "response": r.reason, "text": await r.text}
+			return {"code": r.status, "response": r.reason, "text": await r.text()}
 
 def _values():
 	return {
@@ -37,11 +33,11 @@ def _values():
 			"str",
 			n_map_type.with_typevars(["str", "str"]),
 			n_map_type.with_typevars(["str", "str"]),
-			{"code": "int", "reason": "str", "text": "str"}
+			{"code": "int", "response": "str", "text": "str"}
 		),
 		"get": (
 			"str",
 			n_map_type.with_typevars(["str", "str"]),
-			n_cmd_type.with_typevars([{"code": "int", "reason": "str", "return": n_map_type.with_typevars(["str", "str"])}])
+			n_cmd_type.with_typevars([{"code": "int", "response": "str", "return": n_map_type.with_typevars(["str", "str"])}])
 		)
 	}
