fd477c8eb8160ad6972eb5c811e7f128cb17f47f
Ash Ranjan <ranjanashvin@gmail.com>
2021-02-08 10:57:27 -0800

    cleaned up some code

--- python/modules.py
+++ python/modules.py
@@ -6,6 +6,11 @@ import libraries.times
 import libraries.websocket
 import sys
 
-libraries = sys.modules
-
-del libraries["sys"]
+libraries = {
+	'libraries.FileIO': libraries.FileIO,
+	'libraries.json': libraries.json,
+	'libraries.request': libraries.request,
+	'libraries.SystemIO': libraries.SystemIO,
+	'libraries.times': libraries.times,
+	'libraries.websocket': libraries.websocket
+}
