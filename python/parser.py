6405a558d7b7b7e9c3c51f2650173844ea764be8
SheepTester <ussa.say@gmail.com>
2021-03-18 11:48:45 -0700

    Move parser into separate (and apparently unused) file

--- python/parser.py
+++ python/parser.py
@@ -1,19 +1,18 @@
-from lark import Lark
 import sys
+import path
+
+from lark import Lark
 import lark
 
-file = "run.n"
-if len(sys.argv) > 1:
-	file = ''.join(sys.argv[1:]) 
+# https://stackoverflow.com/a/4381638
+basepath = ""
+if getattr(sys, "frozen", False):
+	basepath = path.dirname(sys.executable)
+elif __file__:
+	basepath = path.dirname(__file__)
 
-parse = ""
-text = ""
-with open("syntax.lark", "r") as f:
+syntaxpath = path.join(basepath, "syntax.lark")
+with open(syntaxpath, "r") as f:
 	parse = f.read()
-with open(file, "r") as f:
-	text = f.read()
 
-n_parser = Lark(parse, start='start')
-
-print(n_parser.parse(text).pretty())
-print(n_parser.parse(text))
+n_parser = Lark(parse, start="start", propagate_positions=True)
