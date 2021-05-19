738db70a792b278559e09945a5eb0d8e9aea72a2
Ash Ranjan <ranjanashvin@gmail.com>
2021-05-18 19:36:55 -0700

    fix getTime

--- python/libraries/times.py
+++ python/libraries/times.py
@@ -6,11 +6,11 @@ from native_types import n_cmd_type
 async def sleep(time):
     await asyncio.sleep(time / 1000)
 
-async def getTimeSince(time):
-	return t.time() - time
+async def getTime():
+	return t.time()
 
 def _values():
     return {
         "sleep": ("int", n_cmd_type.with_typevars(["unit"])),
-        "getTimeSince": ("float", n_cmd_type.with_typevars(["float"])),
+        "getTime": (n_cmd_type.with_typevars(["float"]),),
     }
