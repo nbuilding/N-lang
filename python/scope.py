be2e075e6e9213e5221940f67a368a5ea4f15538
Ash Ranjan <ranjanashvin@gmail.com>
2021-06-30 20:09:32 -0700

    I think i fixed STRING

--- python/scope.py
+++ python/scope.py
@@ -3,6 +3,7 @@ import os.path
 import sys
 
 import lark
+import regex as re
 from lark import Lark
 from colorama import Fore, Style
 
@@ -181,7 +182,7 @@ def unescape_sequence(escape_sequence_match):
         return escape_sequence_match[3]
 
 def unescape(string):
-    return re.replace(r'\\(?:([nrtv0fb"\\])|u\{([0-9a-fA-F]+)\}|\{(.)\})"', unescape_sequence, string)
+    return re.sub(r'\\(?:([nrtv0fb"\\])|u\{([0-9a-fA-F]+)\}|\{(.)\})"', unescape_sequence, string)
 
 class Scope:
     def __init__(
@@ -798,7 +799,7 @@ class Scope:
                 return float(value)
             return int(value)
         elif value.type == "STRING":
-            return bytes(unescape_sequence(value[1:-1]), "utf-8").decode("unicode_escape")
+            return bytes(unescape(value[1:-1]), "utf-8").decode("unicode_escape")
         elif value.type == "BOOLEAN":
             if value.value == "false":
                 return False
@@ -994,7 +995,7 @@ class Scope:
                     raise SyntaxError("Unexpected escape code: %s" % code)
             else:
                 if (val.type == "HEX"):
-                    return eval_value(val.value)
+                    return self.eval_value(val)
                 return val.value
         elif expr.data == "value":
             token_or_tree = expr.children[0]
@@ -1004,7 +1005,7 @@ class Scope:
                 return self.eval_value(token_or_tree)
         elif expr.data == "impn":
             if expr.children[0].type == "STRING":
-                rel_file_path = bytes(eval_value(expr.children[0].value[1:-1]), "utf-8").decode(
+                rel_file_path = bytes(unescape(expr.children[0].value[1:-1]), "utf-8").decode(
                     "unicode_escape"
                 )
             else:
@@ -1882,7 +1883,7 @@ class Scope:
                 return n_list_type.with_typevars([contained_type])
         elif expr.data == "impn":
             if expr.children[0].type == "STRING":
-                rel_file_path = bytes(unescape_sequence(expr.children[0].value[1:-1]), "utf-8").decode(
+                rel_file_path = bytes(unescape(expr.children[0].value[1:-1]), "utf-8").decode(
                     "unicode_escape"
                 )
             else:
