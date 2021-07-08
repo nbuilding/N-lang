8c95b488530800bebd1b0c5b42a4364558b8c0a2
Ash Ranjan <ranjanashvin@gmail.com>
2021-07-07 22:11:56 -0700

    add value access

--- python/operation_types.py
+++ python/operation_types.py
@@ -1,5 +1,5 @@
 from type import NTypeVars, NGenericType
-from native_types import n_list_type, list_generic, n_map_type
+from native_types import n_list_type, list_generic, n_map_type, n_maybe_type
 
 # Might move these into Scope one day because these might be scoped due to
 # implementations of traits.
@@ -8,6 +8,10 @@ from native_types import n_list_type, list_generic, n_map_type
 in_generic = NGenericType("t")
 in_map_generic = NGenericType("k")
 in_value_generic = NGenericType("v")
+
+access_list_generic = NGenericType("t")
+access_map_generic = NGenericType("k")
+access_value_generic = NGenericType("v")
 
 binary_operation_types = {
     "OR": [("bool", "bool", "bool"), ("int", "int", "int")],
@@ -40,6 +44,42 @@ binary_operation_types = {
     # Exponents are weird because negative powers result in non-integers.
     # TODO: Make int ^ int an int; negative powers should result in 0.
     "EXPONENT": [("int", "int", "float"), ("float", "float", "float")],
+    "VALUEACCESS": [
+        (
+            n_list_type.with_typevars([access_list_generic]),
+            "int",
+            n_maybe_type.with_typevars([access_list_generic]),
+        ), (
+            n_map_type.with_typevars([
+                access_map_generic,
+                access_value_generic,
+            ]),
+            access_map_generic,
+            n_maybe_type.with_typevars([access_value_generic]),
+        ),
+        (
+            "str",
+            "int",
+            "char",
+        ),
+        (
+            n_maybe_type.with_typevars([n_list_type.with_typevars([access_list_generic])]),
+            "int",
+            n_maybe_type.with_typevars([access_list_generic]),
+        ), (
+            n_maybe_type.with_typevars([n_map_type.with_typevars([
+                access_map_generic,
+                access_value_generic,
+            ])]),
+            access_map_generic,
+            n_maybe_type.with_typevars([access_value_generic]),
+        ),
+        (
+            n_maybe_type.with_typevars(["str"]),
+            "int",
+            n_maybe_type.with_typevars(["char"]),
+        ),
+    ]
 }
 unary_operation_types = {
     "SUBTRACT": [("int", "int"), ("float", "float")],
