ba52ffe1ed57f6abe7cbe60d6d5911df34ffbb7d
Ash Ranjan <ranjanashvin@gmail.com>
2021-09-07 22:19:39 -0700

    generalized request function

--- python/libraries/request.py
+++ python/libraries/request.py
@@ -9,15 +9,15 @@ from flask import Flask, request as req, Response
 from flask.logging import default_handler
 from werkzeug.wsgi import FileWrapper
 
-from native_types import n_cmd_type, NMap, n_map_type, n_list_type
-from libraries.json import json_value_type, python_to_json, string
+from native_types import n_cmd_type, NMap, n_map_type, n_list_type, none, n_maybe_type
+from libraries.json import json_value_type, python_to_json, string, json_to_python
 from ncmd import Cmd
 
 
-async def get(url, headers):
+async def request(request_type, url, headers, data):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers) as r:
+            async with session.request(request_type, url, headers=json_to_python(headers.values[0]) if headers.variant == "yes" else None, data=json_to_python(data.values[0]) if data.variant == "yes" else None) as r:
                 returndata = string(await r.text())
                 try:
                     returndata = python_to_json(json.loads(await r.text()))
@@ -30,100 +30,6 @@ async def get(url, headers):
             "response": "Cannot connect",
             "return": string(str(err)),
         }
-
-
-async def delete(url, headers):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.delete(url, headers=headers) as r:
-                returndata = string(await r.text())
-                try:
-                    returndata = python_to_json(json.loads(await r.text()))
-                except:
-                    pass
-                return {"code": r.status, "response": r.reason, "return": returndata}
-    except aiohttp.client_exceptions.ClientError as err:
-        return {
-            "code": 400,
-            "response": "Cannot connect",
-            "return": string(str(err)),
-        }
-
-
-async def head(url, headers):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.head(url, headers=headers) as r:
-                returndata = string(await r.text())
-                try:
-                    returndata = python_to_json(json.loads(await r.text()))
-                except:
-                    pass
-                return {"code": r.status, "response": r.reason, "return": returndata}
-    except aiohttp.client_exceptions.ClientError as err:
-        return {
-            "code": 400,
-            "response": "Cannot connect",
-            "return": string(str(err)),
-        }
-
-
-async def options(url, headers):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.options(url, headers=headers) as r:
-                returndata = string(await r.text())
-                try:
-                    returndata = python_to_json(json.loads(await r.text()))
-                except:
-                    pass
-                return {"code": r.status, "response": r.reason, "return": returndata}
-    except aiohttp.client_exceptions.ClientError as err:
-        return {
-            "code": 400,
-            "response": "Cannot connect",
-            "return": string(str(err)),
-        }
-
-
-async def post(url, content, headers):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, data=json.dumps(content), headers=headers) as r:
-                return {"code": r.status, "response": r.reason, "text": await r.text()}
-    except aiohttp.client_exceptions.ClientError as err:
-        return {
-            "code": 400,
-            "response": "Cannot connect",
-            "text": str(err),
-        }
-
-
-async def patch(url, content, headers):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.patch(url, data=json.dumps(content), headers=headers) as r:
-                return {"code": r.status, "response": r.reason, "text": await r.text()}
-    except aiohttp.client_exceptions.ClientError as err:
-        return {
-            "code": 400,
-            "response": "Cannot connect",
-            "text": str(err),
-        }
-
-
-async def put(url, content, headers):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.put(url, data=json.dumps(content), headers=headers) as r:
-                return {"code": r.status, "response": r.reason, "text": await r.text()}
-    except aiohttp.client_exceptions.ClientError as err:
-        return {
-            "code": 400,
-            "response": "Cannot connect",
-            "text": str(err),
-        }
-
 
 async def createServer(port, page_handler):
     app = Flask(__name__)
@@ -154,56 +60,13 @@ async def createServer(port, page_handler):
 
 def _values():
     return {
-        "post": (
+        "request": (
             "str",
-            n_map_type.with_typevars(["str", "str"]),
-            n_map_type.with_typevars(["str", "str"]),
-            n_cmd_type.with_typevars(
-                [{"code": "int", "response": "str", "text": "str"}]
-            ),
-        ),
-        "get": (
             "str",
-            n_map_type.with_typevars(["str", "str"]),
+            n_maybe_type.with_typevars([json_value_type]),
+            n_maybe_type.with_typevars([json_value_type]),
             n_cmd_type.with_typevars(
                 [{"code": "int", "response": "str", "return": json_value_type}]
-            ),
-        ),
-        "delete": (
-            "str",
-            n_map_type.with_typevars(["str", "str"]),
-            n_cmd_type.with_typevars(
-                [{"code": "int", "response": "str", "return": json_value_type}]
-            ),
-        ),
-        "head": (
-            "str",
-            n_map_type.with_typevars(["str", "str"]),
-            n_cmd_type.with_typevars(
-                [{"code": "int", "response": "str", "return": json_value_type}]
-            ),
-        ),
-        "options": (
-            "str",
-            n_map_type.with_typevars(["str", "str"]),
-            n_cmd_type.with_typevars(
-                [{"code": "int", "response": "str", "return": json_value_type}]
-            ),
-        ),
-        "patch": (
-            "str",
-            n_map_type.with_typevars(["str", "str"]),
-            n_map_type.with_typevars(["str", "str"]),
-            n_cmd_type.with_typevars(
-                [{"code": "int", "response": "str", "text": "str"}]
-            ),
-        ),
-        "put": (
-            "str",
-            n_map_type.with_typevars(["str", "str"]),
-            n_map_type.with_typevars(["str", "str"]),
-            n_cmd_type.with_typevars(
-                [{"code": "int", "response": "str", "text": "str"}]
             ),
         ),
         "createServer": (
